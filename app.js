79bd135fbd480a70a71cf69fe1ec2fe22e86987b
NahomKibreab <nknahom@gmail.com>
2020-12-26 23:51:29 -0700

    changed to one while loop

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 let todoList = [];
 let request = prompt('What would you like to do?');
-if (request !== 'quit') {
-  while (true) {
-    if (request === 'new') {
-      while (true) {
-        request = prompt('Enter new list:');
-        if (request === 'quit') break;
-        todoList.push(request);
-        console.log(`${request} added to list`);
-      }
-    } else if (request === 'list') {
-      console.log('**********');
-      let count = 0;
-      for (let todo of todoList) {
-        console.log(`${count}: ${todo}`);
-        count++;
-      }
-      console.log('**********');
-    } else if (request === 'delete') {
-      let itemIndex = parseInt(prompt('Enter the index you want to delete!'));
-      if (itemIndex >= 0) {
-        todoList.splice(itemIndex, 1);
-      }
+
+while (request !== 'quit' && request !== 'q') {
+  if (request === 'new') {
+    request = prompt('Ok, what is the new todo?');
+    todoList.push(request);
+    console.log(`${request} added to list`);
+  } else if (request === 'list') {
+    console.log('**********');
+    let count = 0;
+    for (let i = 0; i < todoList.length; i++) {
+      console.log(`${i}: ${todoList[i]}`);
     }
-    request = prompt('What would you like to do?');
-    if (request === 'quit') break;
+    console.log('**********');
+  } else if (request === 'delete') {
+    let itemIndex = parseInt(prompt('Enter the index you want to delete!'));
+    if (itemIndex >= 0) {
+      todoList.splice(itemIndex, 1);
+    } else {
+      console.log('Unknown index');
+    }
   }
+  request = prompt('What would you like to do?');
 }
+console.log('OK QUIT THE APP!');
